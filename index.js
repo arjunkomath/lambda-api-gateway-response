12e9ba2560b16b1f24ab758f0a6e0916e370b9b5
Arjun Komath <arjunkomath@gmail.com>
2017-11-09 23:16:35 +0530

    Init

---
+++ index.js
@@ -0,0 +1,60 @@
+/**
+ * API Gateway Response builder for AWS Lambda
+ *
+ * @summary API Gateway Response builder for AWS Lambda
+ * @author Arjun Komath <arjunkomath@gmail.com>
+ *
+ * Created at     : 2017-11-09 22:07:27 
+ * Last modified  : 2017-11-09 22:10:21
+ */
+
+var ApiGatewayResponse = function (callback) {
+
+    // Lambda callback function
+    this.callback = callback;
+
+    // Default response
+    this.response = {
+        statusCode: 200,
+        headers: {
+            "access-control-allow-origin": "*"
+        },
+        body: null
+    };
+
+    /**
+     * HTTP status code
+     * @param {number} statusCode 
+     */
+    this.status = function (statusCode) {
+        this.response.statusCode = statusCode;
+        return this;
+    };
+
+    /**
+     * Headers object
+     * @param {object} headers 
+     */
+    this.headers = function (headers) {
+        this.response.headers = headers;
+        return this;
+    };
+
+    /**
+     * Response Body
+     * @param {*} body 
+     */
+    this.body = function (body) {
+        this.response.body = JSON.stringify(body);
+        return this;
+    };
+
+    /**
+     * Invoke lambda callback
+     */
+    this.send = function () {
+        return this.callback(null, this.response);
+    };
+};
+
+module.exports = ApiGatewayResponse;
---
+++ test/test.js
@@ -0,0 +1,97 @@
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+var ApiGatewayResponse = require('../index');
+
+var callback = function (err, response) {
+    return response;
+};
+
+describe('API Gateway Response', function () {
+
+    describe('200 JSON response', function () {
+
+        var testResponse = new ApiGatewayResponse(callback)
+            .body({
+                "foo": "bar"
+            })
+            .send();
+
+        it('should be an object', function () {
+            expect(testResponse).to.be.a('object');
+        });
+
+        it('should have 200 status', function () {
+            expect(testResponse).to.have.property('statusCode');
+            expect(testResponse.statusCode).equals(200);
+        });
+
+        it('should have JSON body', function () {
+            expect(testResponse).to.have.property('body');
+            expect(testResponse.body).to.be.a('string');
+            expect(JSON.parse(testResponse.body)['foo']).equals('bar');
+        });
+
+    });
+
+    describe('400 JSON response', function () {
+
+        var testResponse = new ApiGatewayResponse(callback)
+            .status(400)
+            .body({
+                'foo': 'bar'
+            })
+            .send();
+
+        it('should be an object', function () {
+            expect(testResponse).to.be.a('object');
+        });
+
+        it('should have 400 status', function () {
+            expect(testResponse).to.have.property('statusCode');
+            expect(testResponse.statusCode).equals(400);
+        });
+
+        it('should have JSON body', function () {
+            expect(testResponse).to.have.property('body');
+            expect(testResponse.body).to.be.a('string');
+            expect(JSON.parse(testResponse.body)['foo']).equals('bar');
+        });
+
+    });
+
+    describe('200 JSON response with custom header', function () {
+
+        var testResponse = new ApiGatewayResponse(callback)
+            .headers({
+                'x-access-token': 'test'
+            })
+            .body({
+                "foo": "bar"
+            })
+            .send();
+
+        it('should be an object', function () {
+            expect(testResponse).to.be.a('object');
+        });
+
+        it('should have 200 status', function () {
+            expect(testResponse).to.have.property('statusCode');
+            expect(testResponse.statusCode).equals(200);
+        });
+
+        it('should have header x-access-token', function () {
+            expect(testResponse).to.have.property('headers');
+            expect(testResponse.headers).to.be.a('object');
+            expect(testResponse.headers['x-access-token']).equal('test');
+        });
+
+        it('should have JSON body', function () {
+            expect(testResponse).to.have.property('body');
+            var data = JSON.parse(testResponse.body);
+            assert.equal(JSON.stringify(data), JSON.stringify({ "foo": "bar" }));
+        });
+
+    });
+
+});
